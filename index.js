3c90b38268e50d85d061621e720db735026d50e7
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2019-06-25 09:51:46 -0300

    add tasks update

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require("express");
 
 const server = express();
 
+// indica para o servidor usar json
 server.use(express.json());
 
+// nossa base de dados
 const projects = [
   { id: 1, title: "Projeto 01", tasks: [] },
   { id: 12, title: "Projeto 12", tasks: [] },
   { id: 3, title: "Projeto 03", tasks: [] }
 ];
 
+// rota de listagem de projetos
 server.get("/projects", (req, res) => {
   res.json(projects);
 });
 
+// rota de criação de projetos
 server.post("/projects", (req, res) => {
   const tasks = [];
   const { id, title } = req.body;
@@ -23,6 +27,7 @@ server.post("/projects", (req, res) => {
   return res.json(projects);
 });
 
+// rota de atualização do title de projetos
 server.put("/projects/:id", (req, res) => {
   const passedId = req.params.id;
   const { title } = req.body;
@@ -40,6 +45,26 @@ server.put("/projects/:id", (req, res) => {
 
   return res.json(projects);
 });
+// rota de criação das tasks de projetos
+server.post("/projects/:id/tasks", (req, res) => {
+  const passedId = req.params.id;
+  const { title } = req.body;
+
+  const getIndex = projects
+    .map((elm, index) => {
+      if (projects[index].id == passedId) {
+        return index;
+      }
+      return "";
+    })
+    .join("");
+
+  projects[getIndex].tasks.push(title);
+
+  return res.json(projects);
+});
+
+// rota de remoção de projetos
 server.delete("/projects/:id", (req, res) => {
   const passedId = req.params.id;
   const { title } = req.body;
@@ -58,4 +83,5 @@ server.delete("/projects/:id", (req, res) => {
   return res.json(projects);
 });
 
+// servidor deve rodar na porta 3000
 server.listen(3000);
